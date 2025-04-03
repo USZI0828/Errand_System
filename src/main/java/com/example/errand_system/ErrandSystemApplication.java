066628ac24598cb4f8debeafc2bed2b1adaf1608
张明阳 <12999464+apexxz@user.noreply.gitee.com>
2025-04-03 15:26:36 +0800

    Initial commit

---
+++ src/main/java/com/example/errand_system/ErrandSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.errand_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrandSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrandSystemApplication.class, args);
+    }
+
+}
